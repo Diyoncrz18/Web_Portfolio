c02fede57f8afe03fada5b90172155480d23dae6
Diyoncrz18 <dkobi16@gmail.com>
2024-11-13 23:15:57 +0800

    Create animation Loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// src/App.js
+import React, { useState, useEffect } from "react";
 import "./assets/css/style.css";
 import Header from "./components/header";
 import Home from "./components/home";
@@ -6,17 +8,35 @@ import Skill from "./components/skill";
 import Certificate from "./components/certificate";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
+import Loading from "./components/loading"; // Import komponen loading
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Set delay untuk simulasi loading
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Loading selama 3 detik, bisa sesuaikan waktunya
+
+    return () => clearTimeout(timer); // Bersihkan timeout saat komponen di-unmount
+  }, []);
+
   return (
     <>
-      <Header />
-      <Home />
-      <About />
-      <Skill />
-      <Certificate />
-      <Contact />
-      <Footer />
+      {isLoading ? (
+        <Loading /> // Menampilkan komponen loading jika masih loading
+      ) : (
+        <>
+          <Header />
+          <Home />
+          <About />
+          <Skill />
+          <Certificate />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
---
+++ src/components/loading/index.js
@@ -0,0 +1,19 @@
+// src/components/loading.js
+import React from "react";
+import "./style.css"
+
+const Loading = () => {
+  return (
+    <div className="container loader">
+      <span>L</span>
+      <span>O</span>
+      <span>A</span>
+      <span>D</span>
+      <span>I</span>
+      <span>N</span>
+      <span>G</span>
+    </div>
+  );
+};
+
+export default Loading;
